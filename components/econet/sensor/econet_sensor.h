df46f41dfaa271188795b7f830b68cafc5b6dcee
Daniel <47092714+Daniel-dev22@users.noreply.github.com>
2023-07-02 06:36:54 -0400

    Update econet_sensor.h

--- components/econet/sensor/econet_sensor.h
+++ components/econet/sensor/econet_sensor.h
@@ -72,7 +72,7 @@ class EconetSensor : public PollingComponent, public EconetClient {
 	void set_cc_automode_sensor(sensor::Sensor *sensor) {
 		this->cc_automode_sensor_ = sensor;
 	}
-  void set_cc_rel_hum_sensor(sensor::Sensor *sensor) {
+        void set_cc_rel_hum_sensor(sensor::Sensor *sensor) {
 		this->cc_rel_hum_sensor_ = sensor;
 	}
 	
