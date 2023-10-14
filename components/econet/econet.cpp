248a2e89ec47d618301ee4cfeb66b31d49579492
tronikos <tronikos@users.noreply.github.com>
2023-10-13 22:09:02 -0700

    Log date time

--- components/econet/econet.cpp
+++ components/econet/econet.cpp
@@ -314,6 +314,9 @@ void Econet::parse_message_(bool is_tx) {
           ESP_LOGI(TAG, "  Unexpected Write Data Length", datatype);
         }
       }
+    } else if (type == 7) {
+      ESP_LOGI(TAG, "  DateTime: %04d/%02d/%02d %02d:%02d:%02d\n", pdata[9] | pdata[8] << 8, pdata[7], pdata[6],
+               pdata[5], pdata[4], pdata[3]);
     }
   }
 }
