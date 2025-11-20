1d3507a6c18b09b3e477df5da36f9c59a4869882
tronikos <tronikos@users.noreply.github.com>
2025-11-19 19:55:31 -0800

    Fix compilation with 2025.11.0 (#541)
    Co-authored-by: Josh Barnard <barndawgie@users.noreply.github.com>

--- components/econet/select/econet_select.cpp
+++ components/econet/select/econet_select.cpp
@@ -40,9 +40,9 @@ void EconetSelect::dump_config() {
   LOG_SELECT("", "Econet Select", this);
   ESP_LOGCONFIG(TAG, "  Select has datapoint ID %s", this->select_id_.c_str());
   ESP_LOGCONFIG(TAG, "  Options are:");
-  auto options = this->traits.get_options();
-  for (auto i = 0; i < this->mappings_.size(); i++) {
-    ESP_LOGCONFIG(TAG, "    %i: %s", this->mappings_.at(i), options.at(i).c_str());
+  const auto &options = this->traits.get_options();
+  for (size_t i = 0; i < this->mappings_.size(); i++) {
+    ESP_LOGCONFIG(TAG, "    %i: %s", this->mappings_.at(i), options.at(i));
   }
 }
 
